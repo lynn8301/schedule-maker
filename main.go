fc4bb2dd4750ca4ccb7fd7053a4231b71b95365c
linnnnnnnnnnnn <lynn8301@gmail.com>
2024-06-04 16:54:03 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
-
 
 func main() {
 	router := gin.Default()
-	router.GET("/test", test)
-	router.Run(":80")
-}
-
-func test(c *gin.Context) {
-	str := []byte("ok")                      // 對於[]byte感到疑惑嗎？ 因為網頁傳輸沒有string的概念，都是要轉成byte字節方式進行傳輸
-	c.Data(http.StatusOK, "text/plain", str) // 指定contentType為 text/plain，就是傳輸格式為純文字啦～
+	// Define a route for the root URL
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
